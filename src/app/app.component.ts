7e785eb7f58fc7310a8548da02d1b8767be28a76
jeniffer-oliveira <100139970+jeniffer-oliveira@users.noreply.github.com>
2022-11-14 16:09:59 -0300

    4 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { BitcoinService } from './bitcoin.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  
+export class AppComponent {
+  name = 'BitCoin';
+
+  myName: string = 'Jeniffer Campos de Oliveira';
+  RA: string = '0050832021036';
+
+  constructor(public bitcoinService: BitcoinService) {}
+
+  ngOnInit() {
+    this.update();
+  }
+
+  getCurrentPrice() {
+    return this.bitcoinService.currentPrice;
+  }
+
+  update() {
+    this.bitcoinService.update();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BitcoinService } from './bitcoin.service';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
+  providers: [BitcoinService],
 })
 export class AppModule {}
